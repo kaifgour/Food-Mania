6111ab6b2449d7a7cff458225d0b7a865e6efaff
kaifgour <kaifi252000@gmail.com>
2024-03-17 14:59:54 +0530

    API fixed

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,7 +18,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2083953&lng=72.8334541&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
